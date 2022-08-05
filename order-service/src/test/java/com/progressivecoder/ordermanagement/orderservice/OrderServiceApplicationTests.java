22c9fc87eacc55166e4d48adfcb8b93bcfed5a22
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-08-05 09:26:35 +0530

    first commit

---
+++ order-service/src/test/java/com/progressivecoder/ordermanagement/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.progressivecoder.ordermanagement.orderservice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class OrderServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
